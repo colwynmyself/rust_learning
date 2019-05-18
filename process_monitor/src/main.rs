b4e0e3ac586cd8c29b9620deade690ef55ac025b
Colwyn Fritze-Moor <colwyn@colwyn.me>
2019-05-18 11:34:01 -0700

    Run rustfmt

--- process_monitor/src/main.rs
+++ process_monitor/src/main.rs
@@ -1,15 +1,18 @@
-mod system;
 mod process;
+mod system;
 
 fn main() {
-  let mut system = system::get_system();
+    let mut system = system::get_system();
 
-  let processes = system::get_processes(&mut system);
+    let processes = system::get_processes(&mut system);
 
-  for process in processes {
-    let memory_string = process.get_memory_string();
-    let cpu_string = process.get_cpu_string();
+    for process in processes {
+        let memory_string = process.get_memory_string();
+        let cpu_string = process.get_cpu_string();
 
-    println!("{} {} ({} / {})", process.pid, process.name, memory_string, cpu_string);
-  }
+        println!(
+            "{} {} ({} / {})",
+            process.pid, process.name, memory_string, cpu_string
+        );
+    }
 }
